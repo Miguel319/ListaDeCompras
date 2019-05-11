b9130231c5fdd08983120acc17716a67d963b6de
Miguel319 <miguelp90.319@gmail.com>
2019-05-10 20:19:38 -0400

    Agregar/actualizar/eliminar artículos

--- src/app/compras-lista/compras-editar/compras-editar.component.ts
+++ src/app/compras-lista/compras-editar/compras-editar.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs";
+import { Articulo } from "src/app/modelos/articulo.model.";
+import { ArticuloService } from "src/app/servicio/articulo.service";
 
 @Component({
-  selector: 'app-compras-editar',
-  templateUrl: './compras-editar.component.html',
-  styleUrls: ['./compras-editar.component.css']
+  selector: "app-compras-editar",
+  templateUrl: "./compras-editar.component.html",
+  styleUrls: ["./compras-editar.component.css"]
 })
-export class ComprasEditarComponent implements OnInit {
+export class ComprasEditarComponent implements OnInit, OnDestroy {
+  @ViewChild("f") comprasListaForm: NgForm;
+  suscripcion: Subscription;
+  modoEditar = false;
+  articuloEditadoIndex: number;
+  articuloEditado: Articulo;
 
-  constructor() { }
+  constructor(private articuloService: ArticuloService) {}
 
   ngOnInit() {
+    this.suscripcion = this.articuloService.inicioEdicion.subscribe(
+      (indice: number) => {
+        this.articuloEditadoIndex = indice;
+        this.modoEditar = true;
+        this.articuloEditado = this.articuloService.getArticulo(indice);
+        this.comprasListaForm.setValue({
+          nombre: this.articuloEditado.nombre,
+          cantidad: this.articuloEditado.cantidad,
+          precio: this.articuloEditado.precio
+        });
+      }
+    );
   }
 
+  ngOnDestroy() {
+    this.suscripcion.unsubscribe();
+    this.modoEditar = false;
+  }
+
+  alLimpiar() {
+    this.comprasListaForm.reset();
+    this.modoEditar = false;
+  }
+
+  alEliminar() {
+    this.articuloService.eliminarArticulo(this.articuloEditadoIndex);
+    this.alEliminar();
+  }
+
+  alEnviar(formulario: NgForm) {
+    const valor = formulario.value;
+    const nuevoArticulo = new Articulo(
+      valor.nombre,
+      valor.cantidad,
+      valor.precio
+    );
+
+    if (this.modoEditar) {
+      this.articuloService.actualizarArticulo(
+        this.articuloEditadoIndex,
+        nuevoArticulo
+      );
+    } else {
+      this.articuloService.agregarArticulo(nuevoArticulo);
+    }
+
+    this.modoEditar = false;
+    formulario.reset();
+  }
 }
